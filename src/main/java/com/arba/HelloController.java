3a083ee4338c3319dc4170ee5fae7763182bb205
soonly <soonly.lin@arbaholdings.com.hk>
2019-08-27 14:46:44 +0800

    '002'

--- src/main/java/com/arba/HelloController.java
+++ src/main/java/com/arba/HelloController.java
@@ -1,15 +1,12 @@
 package com.arba;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
-// @RestController
+@RestController
 public class HelloController {
-        @RequestMapping("/hello")
-        public String hello(){
-            return "HelloController中的hello方法";
-        }
+	@GetMapping("/hello")
+	public String hello() {
+		return "HelloController中的hello方法";
+	}
 }
